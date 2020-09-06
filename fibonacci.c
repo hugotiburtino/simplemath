05bbe77f55a6fa1eccdc92efe55a854a1f245be7
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-09-06 21:52:36 +0200

    Change types in order to accomodate larger calculation

--- fibonacci.c
+++ fibonacci.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main (void) {
-  int i, n, f1 = 0, f2 = 1, fn;
+  int n;
+  unsigned long fn, f1 = 0, f2 = 1;
 
   printf("Give a value for n: ");
   scanf("%i", &n);
 
-  for(i=1; i < n; ++i){
+  for(int i=1; i < n; ++i){
       fn = f1 + f2;
       f1 = f2;
       f2 = fn;
     }
 
-  printf("Fib(%i) = %i", n, fn);
+  printf("Fib(%i) = %lu", n, fn);
   return 0;
 }
